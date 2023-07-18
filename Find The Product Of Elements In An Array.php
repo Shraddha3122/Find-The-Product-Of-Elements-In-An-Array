d1efa9b076103b80a9dd5c748ef0dd0dc74db3ad
Shraddha Hukkeri <113444373+Shraddha3122@users.noreply.github.com>
2023-07-18 08:19:33 +0530

    PHP Added

---
+++ Find The Product Of Elements In An Array.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php    
+$numbers=array("10","20","30","40");    
+echo "Product of array elements is: ";
+echo array_product($numbers);
+?>    
+ 
+</body>
+</html>
